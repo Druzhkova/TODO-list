eb3b686a1f7d6ea73a3e16f75e04208db464c1aa
Viktoriya <druzhkova.vika.v@gmail.com>
2020-12-18 19:17:39 +0300

    feat: add component button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from './components';
 
 function App() {
   return (
     <div>
+      <Button type="transperent" color='skyblue'>Click</Button>
     </div>
   );
 }

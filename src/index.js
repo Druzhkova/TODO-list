8da02de3f9069409f0bf75575685b81fb8fa325a
Viktoriya <druzhkova.vika.v@gmail.com>
2020-12-31 18:08:54 +0300

    feat: add hash-router

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={store }>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
